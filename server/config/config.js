45db889d4ec8455d47ce02206cf41fcca2f8a77c
Edgar Ulises Navarrete Valencia <edgar.navarrete@mobiik.com>
2019-08-02 13:02:40 -0500

    valiable entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://seiskari:e4HmpYGn8cfrttd@cluster0-nomvr.mongodb.net/cafe';
+    // urlDB = 'mongodb+srv://seiskari:e4HmpYGn8cfrttd@cluster0-nomvr.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
