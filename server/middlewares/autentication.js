77280b8bfef854b987b72ff4e9bf4646d0b74c2e
Edgar Ulises Navarrete Valencia <edgar.navarrete@mobiik.com>
2019-08-12 13:03:22 -0500

    Implementacion de imagenes

--- server/middlewares/autentication.js
+++ server/middlewares/autentication.js
@@ -37,8 +37,28 @@ let verificaAdmin_Role = (req, res, next) => {
     }
 };
 
+// ========================
+// Verificar token para imagenes
+// ========================
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido'
+                }
+            });
+        }
+
+        req.usuario = decoded.usuario;
+        next();
+    })
+}
 
 module.exports = {
     verificatoken,
-    verificaAdmin_Role
+    verificaAdmin_Role,
+    verificaTokenImg
 }
